f88cb48c5f44a3103402cc90b1c6b47da69f48c5
Jeff Perkinson <jmperkinson@gmail.com>
2020-05-07 21:06:41 -0400

    Add the initial store of game phrases.
    This commit simply adds a one-dimensional array
    of phrases that will serve as the puzzles
    for the game.

---
+++ inc/phraseStore.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Phrase store for game phrases
+ * Taken from a list of the most common English idioms 
+ * https://www.ef.edu/english-resources/english-idioms/
+ */ 
+
+$phrases = [
+
+    "A blessing in disguise",
+    "A dime a dozen",
+    "Beat around the bush",
+    "Better late than never",
+    "Bite the bullet",
+    "Break a leg",
+    "Easy does it",
+    "Get something out of your system",
+    "Give someone the benefit of the doubt",
+    "Hit the sack",
+    "It's not rocket science",
+    "Let someone off the hook",
+    "So far so good",
+    "Speak of the devil",
+    "That's the last straw",
+    "Time flies when you're having fun",
+    "We'll cross that bridge when we get there",
+    "Wrap your head around something",
+    "You can say that again",
+    "Your guess is as good as mine",
+];
