7f2ab09c83e6773672407887a8e2bfb6566b6d74
Jeff Perkinson <jmperkinson@gmail.com>
2020-05-08 08:37:44 -0400

    Format phrase store for improved readability.

--- inc/phraseStore.php
+++ inc/phraseStore.php
@@ -4,43 +4,112 @@
  * Phrase store for game phrases
  * Taken from a list of the most common English idioms (https://www.ef.edu/english-resources/english-idioms/)
  * and The 100 Most Famous Quotes of All Time (https://blog.hubspot.com/sales/famous-quotes)
- */ 
+ */
 
 $phrases = [
 
-    ["phrase" => "A blessing in disguise", "difficulty" => "medium"],
-    ["phrase" => "A dime a dozen", "difficulty" => "easy"],
-    ["phrase" => "Beat around the bush", "difficulty" => "easy"],
-    ["phrase" => "Better late than never", "difficulty" => "easy"],
-    ["phrase" => "Bite the bullet", "difficulty" => "medium"],
-    ["phrase" => "Break a leg", "difficulty" => "medium"],
-    ["phrase" => "Easy does it", "difficulty" => "medium"],
-    ["phrase" => "Get something out of your system", "difficulty" => "medium"],
-    ["phrase" => "Give someone the benefit of the doubt", "difficulty" => "medium"],
-    ["phrase" => "Hit the sack", "difficulty" => "medium"],
-    ["phrase" => "It's not rocket science", "difficulty" => "easy"],
-    ["phrase" => "Let someone off the hook", "difficulty" => "medium"],
-    ["phrase" => "So far so good", "difficulty" => "easy"],
-    ["phrase" => "Speak of the devil", "difficulty" => "easy"],
-    ["phrase" => "That's the last straw", "difficulty" => "easy"],
-    ["phrase" => "Time flies when you're having fun", "difficulty" => "medium"],
-    ["phrase" => "We'll cross that bridge when we get there", "difficulty" => "medium"],
-    ["phrase" => "Wrap your head around something", "difficulty" => "medium"],
-    ["phrase" => "You can say that again", "difficulty" => "easy"],
-    ["phrase" => "Your guess is as good as mine", "difficulty" => "easy"],
-    ["phrase" => "Life is what happens when you're busy making other plans", 
-                "difficulty" => "hard", 
-                "source" => "John Lennon"],
-    ["phrase" => "The way to get started is to quit talking and begin doing", 
-                "difficulty" => "hard", 
-                "source" => "Walt Disney"],
-    ["phrase" => "The future belongs to those who believe in the beauty of their dreams", 
-                "difficulty" => "hard",
-                "source" => "Elanor Roosevelt"],
-    ["phrase" => "Tell me and I forget. Teach me and I remember. Involve me and I learn", 
-                "difficulty" => "hard",
-                "source" => "Benjamin Franklin"],
-    ["phrase" => "In the end, it's not the years in your life that count. It's the life in your years", 
-                "difficulty" => "hard",
-                "source" => "Abraham Lincoln"]
+    [
+        "phrase" => "A blessing in disguise",
+        "difficulty" => "medium"
+    ],
+    [
+        "phrase" => "A dime a dozen",
+        "difficulty" => "easy"
+    ],
+    [
+        "phrase" => "Beat around the bush",
+        "difficulty" => "easy"
+    ],
+    [
+        "phrase" => "Better late than never",
+        "difficulty" => "easy"
+    ],
+    [
+        "phrase" => "Bite the bullet",
+        "difficulty" => "medium"
+    ],
+    [
+        "phrase" => "Break a leg",
+        "difficulty" => "medium"
+    ],
+    [
+        "phrase" => "Easy does it", 
+        "difficulty" => "medium"
+    ],
+    [
+        "phrase" => "Get something out of your system", 
+        "difficulty" => "medium"
+    ],
+    [
+        "phrase" => "Give someone the benefit of the doubt", 
+        "difficulty" => "medium"
+    ],
+    [
+        "phrase" => "Hit the sack", 
+        "difficulty" => "medium"
+    ],
+    [
+        "phrase" => "It's not rocket science", 
+        "difficulty" => "easy"
+    ],
+    [
+        "phrase" => "Let someone off the hook", 
+        "difficulty" => "medium"
+    ],
+    [
+        "phrase" => "So far so good", 
+        "difficulty" => "easy"
+    ],
+    [
+        "phrase" => "Speak of the devil", 
+        "difficulty" => "easy"
+    ],
+    [
+        "phrase" => "That's the last straw", 
+        "difficulty" => "easy"],
+    [
+        "phrase" => "Time flies when you're having fun", 
+        "difficulty" => "medium"
+    ],
+    [
+        "phrase" => "We'll cross that bridge when we get there", 
+        "difficulty" => "medium"
+    ],
+    [
+        "phrase" => "Wrap your head around something", 
+        "difficulty" => "medium"
+    ],
+    [
+        "phrase" => "You can say that again", 
+        "difficulty" => "easy"
+    ],
+    [
+        "phrase" => "Your guess is as good as mine", 
+        "difficulty" => "easy"
+    ],
+    [
+        "phrase" => "Life is what happens when you're busy making other plans",
+        "difficulty" => "hard",
+        "source" => "John Lennon"
+    ],
+    [
+        "phrase" => "The way to get started is to quit talking and begin doing",
+        "difficulty" => "hard",
+        "source" => "Walt Disney"
+    ],
+    [
+        "phrase" => "The future belongs to those who believe in the beauty of their dreams",
+        "difficulty" => "hard",
+        "source" => "Elanor Roosevelt"
+    ],
+    [
+        "phrase" => "Tell me and I forget. Teach me and I remember. Involve me and I learn",
+        "difficulty" => "hard",
+        "source" => "Benjamin Franklin"
+    ],
+    [
+        "phrase" => "In the end, it's not the years in your life that count. It's the life in your years",
+        "difficulty" => "hard",
+        "source" => "Abraham Lincoln"
+    ]
 ];
