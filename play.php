abedc0e10e721fa04bf4b634ef3be1313311aeec
Jeff Perkinson <jmperkinson@gmail.com>
2020-05-12 09:11:15 -0400

    Add phrase filter by difficulty in play.php.

--- play.php
+++ play.php
@@ -3,12 +3,32 @@
 require_once(__DIR__ . '/inc/config.php');
 require_once(__DIR__ . './views/header.php');
 
+// filter $gamePhrases based on difficulty level selected
+if(isset($_GET["difficulty"])) {
+    if($_GET["difficulty"] == "easy") {
+        $array = $gamePhrases->getAll("1 - easy", "difficulty");
+        var_dump($array);
+    }
+    else if($_GET["difficulty"] == "medium") {
+        $array = $gamePhrases->getAll("2 - medium", "difficulty");
+        var_dump($array);
+    }
+    else if($_GET["difficulty"] == "hard") {
+        $array = $gamePhrases->getAll("3 - hard", "difficulty");
+        var_dump($array);
+    }
+    else {  // player chose random, pull the entire phrase array and create a random phrase object
+        $array = $gamePhrases->getAll();
+        var_dump($array);
+    }    
+}
 
-$testPhrase = new Phrase($gamePhrases);
-$currentGame = new Game($testPhrase);
+$gamePhrase = new Phrase($gamePhrases);
+// var_dump($gamePhrase);
+// $currentGame = new Game($testPhrase);
 
-echo ($testPhrase->getCurrentPhrase());
-var_dump($testPhrase->splitPhrase());
+//echo ($testPhrase->getCurrentPhrase());
+//var_dump($testPhrase->splitPhrase());
 
 ?>
 
