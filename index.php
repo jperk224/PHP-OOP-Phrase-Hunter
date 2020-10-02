70e9578e176bdb771083a31e2053d0f5d542e88d
Jeff Perkinson <jmperkinson@gmail.com>
2020-10-02 09:41:40 -0400

    Fix file path error in index.php.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once(__DIR__ . '/inc/config.php');
-require_once(__DIR__ . './views/header.php');
+require_once(__DIR__ . '/views/header.php');
 
 // variables for form persistence
 $playerName = '';
